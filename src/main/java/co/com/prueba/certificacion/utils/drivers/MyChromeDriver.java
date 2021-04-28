513fcf7a7cbfec16005a7fc84087fb5d031f1749
Camilo Betancur <camilo.betancur@MacBook-Pro-de-SBMEDADMIN.local>
2021-04-28 12:55:21 -0500

    cambio de chromedriver

--- src/main/java/co/com/prueba/certificacion/utils/drivers/MyChromeDriver.java
+++ src/main/java/co/com/prueba/certificacion/utils/drivers/MyChromeDriver.java
@@ -11,7 +11,7 @@ public class MyChromeDriver {
 
     public static MyChromeDriver web(){
         System.setProperty("webdriver.chrome.driver",
-                "src/test/resources/drivers/chromedriver");
+                "src/test/resources/drivers/chromedriver.exe");
 
         String  rutaDesrgaArchivoDefecto = "${user.dir}\\src\\test\\resources\\archivos";
         HashMap<String, Object> chromePreferencias = new HashMap<String, Object>();
